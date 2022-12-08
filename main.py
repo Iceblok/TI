3c2d6182d87add8a9f90b4a7665e716096406934
Iceblok <ycoskun0318@gmail.com>
2022-12-08 19:02:37 +0100

    custom colours for setup

--- main.py
+++ main.py
@@ -6,40 +6,102 @@ import neopixel
 np = neopixel.NeoPixel(machine.Pin(13), 8)
 
 while True:
-    time.sleep(1)
+    time.sleep(0.1)
     np[0] = [255, 0, 0]
     np.write()
-    time.sleep(1)
+    time.sleep(0.1)
     np[1] = [255, 0, 0]
     np.write()
-    time.sleep(1)
+    time.sleep(0.1)
     np[2] = [255, 0, 0]
     np.write()
-    time.sleep(1)
+    time.sleep(0.1)
     np[3] = [255, 0, 0]
     np.write()
-    time.sleep(1)
+    time.sleep(0.1)
     np[4] = [255, 0, 0]
     np.write()
-    time.sleep(1)
+    time.sleep(0.1)
     np[5] = [255, 0, 0]
     np.write()
-    time.sleep(1)
+    time.sleep(0.1)
     np[6] = [255, 0, 0]
     np.write()
-    time.sleep(1)
+    time.sleep(0.1)
     np[7] = [255, 0, 0]
     np.write()
-    time.sleep(1)
-    np[0] = [0, 255, 0]
-    np[1] = [0, 255, 0]
-    np[2] = [0, 255, 0]
-    np[3] = [0, 255, 0]
-    np[4] = [0, 255, 0]
-    np[5] = [0, 255, 0]
-    np[6] = [0, 255, 0]
-    np[7] = [0, 255, 0]
+    time.sleep(0.1)
+    np[0] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[1] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[2] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[3] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[4] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[5] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[6] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[7] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[7] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[6] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[5] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[4] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[3] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[2] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[1] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[0] = [255, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[7] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[6] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[5] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[4] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[3] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[2] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[1] = [0, 0, 0]
+    np.write()
+    time.sleep(0.1)
+    np[0] = [0, 0, 0]
     np.write()
 
-    time.sleep(1)
+
 
